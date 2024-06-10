2f2467bde4c9a217fb478a71bc32dc076b825a79
unknown <parnianashariuon@gmail.com>
2024-06-10 11:57:07 -0700

    turns were considered

--- main2.cpp
+++ main2.cpp
@@ -15,8 +15,11 @@ protected:
 	string UserName;
 	string Password;
 	int BestScore;
+	int LateScore;
+	int Turn;
+	int Color;
 public:
-	User() : ID(0), UserName("0"), Password("0"), BestScore(0){}
+	User() : ID(0), UserName("0"), Password("0"), BestScore(0), Color(0),LateScore(2) {}
 	void getInfo(){
 		int id,t;
 		string temp, username, password,user,bestscore;
@@ -85,6 +88,7 @@ public:
 		}
 		return 0;
 	}
+
 	int getID(){
 		return ID;
 	}
@@ -94,8 +98,27 @@ public:
 	string getPass(){
 		return Password;
 	}
-	int getScore(){
+	int getBestScore(){
 		return BestScore;
+	}
+	int getLateScore(){
+		return LateScore;
+	}
+	void updateScore(int q[9][9],int color){
+		int i,j,count=0;
+		for(i=0;i<8;i++){
+			for(j=0;j<8;j++){
+				if(q[i][j]==color){
+					count+=1;
+				}
+			}
+		}
+		LateScore=count;
+	}	
+	void updateBestScore(int score){
+		if(score>BestScore){
+			BestScore=score;
+		}
 	}
 	
 	void setID(int id){
@@ -107,10 +130,26 @@ public:
 	void setPassword(string password){
 		Password=password;
 	}
-	void setScore(int score){
+	void setBestScore(int score){
 		BestScore=score;
 	}
+	void setTurn(){
+		Turn=1;
+	}
+	void resetTurn(){
+		Turn=0;
+	}
+	int isTurn(){
+		return Turn;
+	}
+	void setColor(int colorNum){
+		Color=colorNum;
+	}
+	int getColor(){
+		return Color;
+	}
 };	
+
 class Admin : public User{
 private:
 	void setData(){
@@ -123,6 +162,7 @@ public:
 		setData();
 	}
 };
+
 class Game{
 public:
 	Game(){
@@ -131,11 +171,11 @@ public:
 	setBoard();
 	}
 	
-	void placePiece(int x, int y, int color) {
+	void placePiece(int x, int y, int color) {	
 	    int centerX = x * CELL_SIZE + CELL_SIZE / 2;
 	    int centerY = y * CELL_SIZE + CELL_SIZE / 2;
 	    setfillstyle(SOLID_FILL, color);
-	    fillellipse(centerX, centerY, CELL_SIZE / 2 - 10, CELL_SIZE / 2 - 10);
+	    fillellipse(centerX, centerY, CELL_SIZE / 2 - 10, CELL_SIZE / 2 - 10);			
 	}
 	void drawBoard() {
 		int i, j;
@@ -201,6 +241,39 @@ public:
 		}
 		return countA+countB;
 	}
+	int checkLine(int x,int y,int color, int q[9][9]){
+		int i,countB=0,countA=0,index=-1;
+		for(i=x-1;i>=0;i--){
+			if(q[i][y]==0){
+				break;
+			}
+			if(q[x][y]==q[i][y]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=index+1;i<x;i++){
+				countB++;
+			}		
+		}
+		index=-1;
+		for(i=x+1;i<8;i++){
+			if(q[i][y]==0){
+				break;
+			}
+			if(q[x][y]==q[i][y]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=x+1;i<index;i++){
+				countA++;
+			}		
+		}
+		return countA+countB;
+	}
 	int flipColumn(int x,int y,int color, int q[9][9]){
 		int i,j,countB=0,countA=0,index=-1;
 		for(j=y-1;j>=0;j--){
@@ -246,6 +319,39 @@ public:
 					q[x][j]=-1;
 					placePiece(x,j,BLACK);
 				}
+			}		
+		}
+		return countA+countB;
+	}
+	int checkColumn(int x,int y,int color, int q[9][9]){
+		int i,j,countB=0,countA=0,index=-1;
+		for(j=y-1;j>=0;j--){
+			if(q[x][j]==0){
+				break;
+			}
+			if(q[x][y]==q[x][j]){
+				index=j;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(j=index+1;j<y;j++){
+				countB++;
+			}		
+		}
+		index=-1;
+		for(j=y+1;j<8;j++){
+			if(q[x][j]==0){
+				break;
+			}
+			if(q[x][y]==q[x][j]){
+				index=j;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(j=y+1;j<index;j++){
+				countA++;
 			}		
 		}
 		return countA+countB;
@@ -350,7 +456,118 @@ public:
 		}
 		return countA+countB;
 	}
+	int checkDiameter(int x,int y,int color,int q[9][9]){
+		int i,j,countB=0,countA=0,countBB=0,countAA=0,index=-1;
+		for(i=1;i<8;i++){
+			if(x+i>=8 || y-i<0 || q[x+i][y-i]==0){
+				break;
+			}
+			if(q[x][y]==q[x+i][y-i]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=1;i<index;i++){
+					countB++;	
+			}
+		}
+		index=-1;
+
+		for(i=1;i<8;i++){
+			if(x-i<0 || y+i>8 || q[x-i][y+i]==0){
+				break;
+			}
+			if(q[x][y]==q[x-i][y+i]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=1;i<index;i++){
+					countA++;
+			}
+		}
+		
+		index=-1;
+
+		for(i=1;i<8;i++){
+			if(x-i<0 || y-i<0 || q[x-i][y-i]==0){
+				break;
+			}
+			if(q[x][y]==q[x-i][y-i]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=1;i<index;i++){
+					countBB++;
+			}
+		}
+		
+		index=-1;
+
+		for(i=1;i<8;i++){
+			if(x+i>=8 || y+i>=8 || q[x+i][y+i]==0){
+				break;
+			}
+			if(q[x][y]==q[x+i][y+i]){
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1){
+			for(i=1;i<index;i++){
+					countAA++;
 	
+			}
+		}
+		return countA+countB+countAA+countBB;
+	}
+	User checkTurn(User a,User b){
+		if(a.isTurn()){
+			return a;
+		}
+		else if(b.isTurn()){
+			return b;
+		}
+	}
+	int passTurn(int q[9][9],int color){
+		int i,j,count=0;
+		for(i=0;i<8;i++){
+			for(j=0;j<8;j++){
+				if(q[i][j]==0){
+					count+=checkLine(i,j,color,q);
+					count+=checkColumn(i,j,color,q);
+					count+=checkDiameter(i,j,color,q);
+				}
+			}
+		}
+		if(count>0){
+			return 0;
+		}
+		return 1;
+	}
+	User getWinner(User user1, User user2){
+		if(user1.getLateScore()>user2.getLateScore()){
+			return user1;
+		}
+		else if(user2.getLateScore()>user1.getLateScore()){
+			return user2;
+		}	
+	}
+	int isBoardFull(int q[9][9]){
+		int r=0,i,j;
+		for(i=0;i<8;i++){
+			for(j=0;j<8;j++){
+				if(q[i][j]==0){
+					return 0;
+				}
+			}
+		}
+		return 1;
+	}
 
 };
 	void highlightCell(int x, int y, int color) {
@@ -383,7 +600,7 @@ vector<User> fillUsers(vector<User> a){
 		temp.setID(id);
 		temp.setUserName(user);
 		temp.setPassword(pass);
-		temp.setScore(score);
+		temp.setBestScore(score);
 		a.push_back(temp);
 	}
 	users.close();
@@ -398,7 +615,7 @@ void fillFile(vector<User> a){
 	users << A.getID() << " " << A.getUsername() << " " << A.getPass();
 	for(i=0;i<a.size();i++){
 		temp=a[i];
-		users << endl << temp.getID() << " " << temp.getUsername() << " " << temp.getPass() << " " << temp.getScore();
+		users << endl << temp.getID() << " " << temp.getUsername() << " " << temp.getPass() << " " << temp.getBestScore();
 	}
 	users.close();
 }
@@ -407,7 +624,7 @@ void showVec(vector <User> b){
 	User temp;
 	for(i=0;i<b.size();i++){
 		temp=b[i];
-		cout << temp.getID() << " " << temp.getUsername() << " " << temp.getPass() << " " << temp.getScore() << endl;
+		cout << temp.getID() << " " << temp.getUsername() << " " << temp.getPass() << " " << temp.getBestScore() << endl;
 	}
 }
 void showGame(int q[9][9]){
@@ -423,7 +640,7 @@ void showGame(int q[9][9]){
 
 int main(){
 	User w;
-	int num,i,j,numb,t;
+	int num,i,j,numb,t,s1=0,s2=0;
 	string user;
 	vector <User> list;
 	list= fillUsers(list);
@@ -442,7 +659,7 @@ int main(){
     int prevX = -1, prevY = -1;
     
 	while(true){
-	cout << "welcome to the othello game!\n" << "Menu:\n" << "1- add player\n" << "2- enter game\n" << "3- review last game\n" << "4- best scores\n" << "5- exit\n";
+	cout << endl << "Menu:\n" << "1- add player\n" << "2- enter game\n" << "3- review last game\n" << "4- best scores\n" << "5- exit\n";
 	cout << "enter option: ";
 	cin >> num;
 		
@@ -461,9 +678,9 @@ int main(){
 			list= fillUsers(list);
 			string user;
 			string user1=getUsername();
+			//admin panel
 			if(user1==a.getUsername()){
 				if(getPassword()==a.getPass()){
-					//admin panel
 					cout << "Welcome Admin!" << endl << "choose option: " << endl << "1- delete user" << endl;
 					cin >> numb;
 					if(numb==1){
@@ -490,12 +707,13 @@ int main(){
 				}
 			}
 			
+			// user panel
 			else{
 				for(i=0;i<list.size();i++){
-					User temp1=list[i];
+					User temp1;
+					temp1=list[i];
 					if(user1==temp1.getUsername()){
 						if(getPassword()==temp1.getPass()){
-							// user panel
 							cout << "welcome user (" << temp1.getUsername() << ")" << endl << "1- single player\n2- two players\nchoose option: ";
 							cin >> numb;
 							if(numb==1){
@@ -509,20 +727,51 @@ int main(){
 									if(user2==temp2.getUsername()){
 										if(temp2.getPass()==getPassword()){
 											cout << "second player confirmed!\n" << "player 1: " << temp1.getUsername() << endl << "player 2: " << temp2.getUsername();
+											cout << "\nplayer 1 plays as BLACK\n";
+											cout << "player 2 plays as WHITE\n";
 											cout << "\npress 1 to start the game...\n";
+											temp1.setColor(-1);
+											temp2.setColor(1);
+											temp1.setTurn();
+											temp2.resetTurn();
 											int start;
 											cin >> start;
-											fflush(stdin);
+
 											if(start==1){
 											cout << "the game starts... ";	
 											Game play;
-											highlightCell(x, y, YELLOW);	
-																	
-										    while (true) {
-										        if (kbhit()) {
+											highlightCell(x, y, YELLOW);
+											User winner;
+											while(true){
+												if(play.isBoardFull(Q)){
+													cout << "\nGAME OVER!\n";
+													temp1.updateScore(Q,-1);
+												    temp2.updateScore(Q,1);
+													temp1.updateBestScore(temp1.getLateScore());
+													temp2.updateBestScore(temp2.getLateScore());
+													winner=play.getWinner(temp1,temp2);
+													for(i=0;i<list.size();i++){
+														User temp=list[i];
+														if(temp.getID()==temp1.getID()){
+															temp.setBestScore(temp1.getBestScore());
+															list[i]=temp;
+														}
+														if(temp.getID()==temp2.getID()){
+															temp.setBestScore(temp2.getBestScore());
+															list[i]=temp;
+														}
+													}
+													fillFile(list);
+													cout << "the winner is: " << play.getWinner(temp1,temp2).getUsername() << endl;
+													cout << "scores:\n" << temp1.getUsername() << ": " << temp1.getLateScore()<< endl;
+													cout << temp2.getUsername() << ": " << temp2.getLateScore() << endl;	
+													cout << "press any key to go back to menu...";
+													getch();
+													closegraph();
+													break;
+												}
+										    if(kbhit()) {
 										            char ch = getch();
-										            if (ch == 0)
-										                ch = getch();
 										            switch (ch) {
 										                case 77:
 										                    if (x < BOARD_SIZE - 1) {
@@ -552,6 +801,44 @@ int main(){
 										                        y++;
 										                    }
 										                    break;
+										                    
+										            	case 'n':{
+										            		if(Q[x][y]==0){
+											            		if(temp1.isTurn()){
+												                    play.placePiece(x, y, BLACK);
+												                    Q[x][y]=-1;
+												                    s1+=play.flipLine(x,y,-1,Q);
+												                    s1+=play.flipColumn(x,y,-1,Q);
+												                    s1+=play.flipDiameter(x,y,-1,Q);
+												                    temp1.resetTurn();
+												                    temp2.setTurn();
+	//											                    if(play.passTurn(Q,1)){
+	//											                    	temp2.resetTurn();
+	//											                   	    temp1.setTurn();
+	//																}
+																}
+																else if(temp2.isTurn()){
+																	play.placePiece(x, y, WHITE);
+												                    Q[x][y]=1;
+												                    s2+=play.flipLine(x,y,1,Q);
+												                    s2+=play.flipColumn(x,y,1,Q);
+												                    s2+=play.flipDiameter(x,y,1,Q);
+												                    temp2.resetTurn();
+												                    temp1.setTurn();
+	//											                    if(play.passTurn(Q,-1)){
+	//										            			temp1.resetTurn();
+	//											                    temp2.setTurn();	
+	//																}
+																}
+//																cout << "\nplayer 1 score: " << temp1.getLateScore();
+//																cout << "\nplayer 2 score: " << temp2.getLateScore();
+												                
+										            			
+															}
+															break;
+														}
+										                    
+										                    
 										                case 'w':
 										                case 'W':
 										                    play.placePiece(x, y, WHITE);
@@ -572,18 +859,20 @@ int main(){
 										                    
 										                case 27:
 										                    closegraph();
-										                    return 0;
+										                    
+										                    
 										            }
+										            
 										            if (prevX != -1) {
 										                highlightCell(prevX, prevY, BLACK);
 										                play.drawBoard();
 										            }
 										            highlightCell(x, y, YELLOW);  
-										        }
-										    }
-										            getch();
-   													 closegraph();
+
+										   	 	}	
 												
+											}
+										   	 	
 											}
 										}
 									}
@@ -595,5 +884,8 @@ int main(){
 			}
 		}
 	
+		if(num==4){
+			
+		}
 	}
 }
